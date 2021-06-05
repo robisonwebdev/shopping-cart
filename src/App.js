35fc1a08c8a53a892bb3d92a590e3369ac28fbaf
David Robison <david@robisonweb.dev>
2021-06-05 12:12:03 -0600

    Imported Home, Shop, and Cart

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Shop from './components/Shop';
+import Cart from './components/Cart';
 
 const App = () => {
   return (
