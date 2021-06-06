2740df0f63a18b2015be7d22c3844d279a582a27
David Robison <david@robisonweb.dev>
2021-06-06 07:38:21 -0600

    Added nav element. Added h3 with temp text. Added ul, with 3 li's.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <div>
-            NavBar
-        </div>
+        <nav>
+            <h3>Temp Site Name</h3>
+            <ul id='navLinks'>
+                <li>Home</li>
+                <li>Shop</li>
+                <li>Cart</li>
+            </ul>
+        </nav>
     );
 };
 
