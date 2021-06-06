76d040c49e8664bc33b4e813fab52629f12055be
David Robison <david@robisonweb.dev>
2021-06-06 07:58:18 -0600

    Changed navTitle to class

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <nav>
-            <Link id='navTitle' to='/'><h3 >Temp Site Name</h3></Link>
+            <Link className='navTitle' to='/'><h3 >Temp Site Name</h3></Link>
             <ul id='navLinks'>
                 <li>Home</li>
                 <li>Shop</li>
