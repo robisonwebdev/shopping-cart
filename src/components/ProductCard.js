0c5b4a83e05710f18d0a8ba027aff2486ab286b3
David Robison <david@robisonweb.dev>
2021-06-07 09:04:06 -0600

    Added className to every element inside div.

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -3,9 +3,9 @@ import React from 'react';
 const ProductCard = () => {
     return (
         <div className='productCard'>
-            <img />
-            <h6></h6>
-            <p></p>
+            <img className='productImage' />
+            <h6 className='productTitle'></h6>
+            <p className='productPrice'></p>
         </div>
     );
 }
