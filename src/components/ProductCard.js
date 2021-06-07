7987420c3fd003a1b94eb5995f83c86edc8d160c
David Robison <david@robisonweb.dev>
2021-06-07 09:59:15 -0600

    Fixed typo on destructed props.

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const ProductCard = (props) => {
-    const [ image, title, price ] = props
+    const { image, title, price } = props;
 
     return (
         <div className='productCard'>
             <img className='productImage' src={image} />
             <h6 className='productTitle'>{title}</h6>
-            <p className='productPrice'>{price}</p>
+            <p className='productPrice'>{`$${price}`}</p>
         </div>
     );
 }
