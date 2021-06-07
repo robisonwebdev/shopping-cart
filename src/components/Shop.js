5d89043398c4d066fb593c2ebdf5b0ddad579e5a
David Robison <david@robisonweb.dev>
2021-06-07 10:00:21 -0600

    Created variable to map data, and called variable inside div

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import ProductCard from './ProductCard';
 import products from '../products/productsInformation';
 
 const Shop = () => {
+    const mappedData = products.map((item) => <ProductCard {...item} key={item.id} />);
+
     return (
         <div className='shop'>
-            Shop
+            {mappedData}
         </div>
     );
 }
