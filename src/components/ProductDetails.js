f4b6493fd9183faca867e9d3ce99d16576b982d8
David Robison <david@robisonweb.dev>
2021-06-09 09:27:51 -0600

    Created function called getProduct to get object from products array. After getProduct get object using match, called setProduct with value. Setup useEffect and calls getProduct.

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,11 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import products from '../products/productsInformation.js';
 
 const ProductDetails = ({ match }) => {
     const [product, setProduct] = useState({});
 
+    useEffect(() => {
+        getProduct();
+        console.log(match);
+    }, []);
+
+    const getProduct = () => {
+        const getProduct = products[match.params.id - 1];
+        setProduct(getProduct);
+    }
+
     return (
         <div>
-            
+            <h1>{product.title}</h1>
         </div>
     );
 }
