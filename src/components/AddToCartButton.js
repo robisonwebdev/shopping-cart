de30a0ec601bbec6cd08e5e1e444934cf4a917a7
David Robison <david@robisonweb.dev>
2021-06-10 09:42:12 -0600

    Removed div and replaced with button

--- src/components/AddToCartButton.js
+++ src/components/AddToCartButton.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 const CheckoutButton = () => {
     return (
-        <div>
-            CheckoutButton
-        </div>
+        <button>Add to cart</button>
     );
 }
 
