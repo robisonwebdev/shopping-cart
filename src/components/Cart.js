5256eaa41ec9b12c6473ad32d7c61fa6d7b1ee82
David Robison <david@robisonweb.dev>
2021-06-11 09:49:31 -0600

    Imported CartCard and tested

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import CartCard from './CartCard';
 
 const Cart = () => {
     return (
         <div className='cart'>
-            
+            <CartCard />
         </div>
     );
 }
