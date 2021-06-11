6a60c43456d1ef9c86096dda55497e4e5b752959
David Robison <david@robisonweb.dev>
2021-06-11 09:48:29 -0600

    Fixed typo

--- src/components/CartCard.js
+++ src/components/CartCard.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const CardCard = () => {
+const CartCard = () => {
     return (
         <div>Cart Card</div>
     );
 }
 
-export default CardCard;
+export default CartCard;
