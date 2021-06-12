80d8cd4d77e632d1a3412c58522de2dcd82ab4fb
David Robison <david@robisonweb.dev>
2021-06-12 10:13:10 -0600

    Added 2 buttons and input type number

--- src/components/QuantityControl.js
+++ src/components/QuantityControl.js
@@ -4,7 +4,9 @@ import '../styles/QuantityControl.css';
 const QuantityControl = () => {
     return (
         <form className='quantityControl'>
-
+            <button>-</button>
+            <input type='number' />
+            <button>+</button>
         </form>
     );
 }
