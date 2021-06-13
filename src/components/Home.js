982b763d9469e981d090421b2816b2219a773bd6
David Robison <david@robisonweb.dev>
2021-06-13 11:06:19 -0600

    Added content in .home

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Home = () => {
     return (
         <div className='home'>
-            Home
+            <h1>Bird Houses</h1>
+            <p>Magna ea irure officia aliquip tempor amet pariatur occaecat nostrud proident qui magna.</p>
+            <button>View Products</button>
         </div>
     );
 }
