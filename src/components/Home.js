aca0771b9ebb2238255642a1228065ae1167b156
David Robison <david@robisonweb.dev>
2021-06-14 09:14:33 -0600

    Replaced home page title

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ import '../styles/Home.css';
 const Home = () => {
     return (
         <div className='home'>
-            <h1>Bird Houses</h1>
+            <h1>Birdie's Bird Houses</h1>
             <p>Magna ea irure officia aliquip tempor amet pariatur occaecat nostrud proident qui magna.</p>
             <Link to='/shop'><button>View Products</button></Link>
         </div>
