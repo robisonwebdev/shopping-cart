9337b6352729754e2d4fcaf422da87c03da15e58
David Robison <david@robisonweb.dev>
2021-06-14 13:57:59 -0600

    Set cart state to Products. Created function to updateProductQuantity and passed down into ProductDetails.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Shop from './components/Shop';
 import Cart from './components/Cart';
 import ProductDetails from './components/ProductDetails';
+import Products from './products/productsInformation';
 import './styles/App.css';
 
 const App = () => {
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(Products);
+
+  useEffect(() => {
+    console.log(cart);
+  }, [cart]);
+
+  function updateProductQuantity(id) {
+    let updateCart = cart.map(product => {
+      if (product.id === id) {
+        return {...product, quantity: product['quantity'] + 1};
+      }
+      return product;
+    });
+
+    setCart(updateCart);
+  }
 
   return (
     <Router>
@@ -25,7 +41,7 @@ const App = () => {
             <Cart/>
           </Route>
           <Route exact path='/shop/:id'>
-            <ProductDetails />
+            <ProductDetails addToCart={updateProductQuantity} />
           </Route>
         </Switch>
       </div>
