6c9fee86dde1d5ea7b47f20a8ff6fc3e9bf8953c
David Robison <david@robisonweb.dev>
2021-06-14 10:20:18 -0600

    Imported useState. Setup a cart/setCart state.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
@@ -8,6 +8,8 @@ import ProductDetails from './components/ProductDetails';
 import './styles/App.css';
 
 const App = () => {
+  const [cart, setCart] = useState([]);
+
   return (
     <Router>
       <div className='App'>
