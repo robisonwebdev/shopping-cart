002a85ccf041383653813f56b1a38bfbee8eba96
David Robison <david@robisonweb.dev>
2021-06-14 12:15:24 -0600

    Added quantity value to objects

--- src/products/productsInformation.js
+++ src/products/productsInformation.js
@@ -4,56 +4,64 @@ const products = [
         image: 'https://images.unsplash.com/photo-1596495551491-734d9ceb1503?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80',
         title: 'Brown House',
         price: '34.00',
-        description: 'Ut pharetra lectus sit amet ante blandit, ac cursus ex porttitor.'
+        description: 'Ut pharetra lectus sit amet ante blandit, ac cursus ex porttitor.',
+        quantity: 0
     },
     {
         id: 2,
         image: 'https://images.unsplash.com/photo-1601305044331-ee8d0c106675?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80',
         title: 'Blue House',
         price: '24.00',
-        description: 'Sed arcu nisl, interdum non maximus ac, ullamcorper vitae nisl.'
+        description: 'Sed arcu nisl, interdum non maximus ac, ullamcorper vitae nisl.',
+        quantity: 0
     },
     {
         id: 3,
         image: 'https://images.unsplash.com/photo-1532704384331-d96a9862ec11?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
         title: 'Moblie House',
         price: '26.00',
-        description: 'In fermentum nulla eu est lacinia, at aliquam dui dictum.'
+        description: 'In fermentum nulla eu est lacinia, at aliquam dui dictum.',
+        quantity: 0
     },
     {
         id: 4,
         image: 'https://images.unsplash.com/photo-1511140016-caa59e0e6d4a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDd8fGJpcmRob3VzZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
         title: 'Flower House',
         price: '25.00',
-        description: 'Donec vitae nisl lobortis, sollicitudin elit a, fermentum lectus. Aenean eu laoreet purus.'
+        description: 'Donec vitae nisl lobortis, sollicitudin elit a, fermentum lectus. Aenean eu laoreet purus.',
+        quantity: 0
     },
     {
         id: 5,
         image: 'https://images.unsplash.com/photo-1591101160264-47328d6bdc9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80',
         title: 'Tree House',
         price: '150.00',
-        description: 'Sed pellentesque urna ac ligula eleifend rutrum. Sed eget malesuada purus, sit amet sagittis nisi. Pellentesque molestie tempus tortor nec sagittis. Donec nec rhoncus augue.'
+        description: 'Sed pellentesque urna ac ligula eleifend rutrum. Sed eget malesuada purus, sit amet sagittis nisi. Pellentesque molestie tempus tortor nec sagittis. Donec nec rhoncus augue.',
+        quantity: 0
     },
     {
         id: 6,
         image: 'https://images.unsplash.com/photo-1613844272478-9e7901305a90?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1029&q=80',
         title: 'Rustic House',
         price: '34.00',
-        description: 'Morbi malesuada tempor suscipit. Aenean venenatis scelerisque magna, eu sodales sem finibus ac. '
+        description: 'Morbi malesuada tempor suscipit. Aenean venenatis scelerisque magna, eu sodales sem finibus ac. ',
+        quantity: 0
     },
     {
         id: 7,
         image: 'https://images.unsplash.com/photo-1554477378-0d7cfb0e8dc7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
         title: 'White House',
         price: '22.00',
-        description: 'Duis tempor sem magna, eu venenatis lectus suscipit ac. Aliquam scelerisque libero quis tellus lacinia, vel pulvinar orci egestas.'
+        description: 'Duis tempor sem magna, eu venenatis lectus suscipit ac. Aliquam scelerisque libero quis tellus lacinia, vel pulvinar orci egestas.',
+        quantity: 0
     },
     {
         id: 8,
         image: 'https://images.unsplash.com/photo-1584216928173-e4cfaf153fef?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=864&q=80',
         title: 'Modern House',
         price: '75.00',
-        description: 'Sed dolor dolor, mattis in mi in, commodo auctor nulla. Mauris interdum pharetra lacinia. Maecenas dapibus enim orci, eu rutrum felis placerat vel. Aenean lacinia feugiat lacus vitae consectetur.'
+        description: 'Sed dolor dolor, mattis in mi in, commodo auctor nulla. Mauris interdum pharetra lacinia. Maecenas dapibus enim orci, eu rutrum felis placerat vel. Aenean lacinia feugiat lacus vitae consectetur.',
+        quantity: 0
     }
 ]
 
