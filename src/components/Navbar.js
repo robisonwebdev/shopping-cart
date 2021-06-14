28169fda7d32a4c592e9d5a77437ae79c8f4d4c6
David Robison <david@robisonweb.dev>
2021-06-14 09:13:53 -0600

    Replaced temp nav title

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <nav>
-            <Link className='navLink' to='/'><h3 >Temp Site Name</h3></Link>
+            <Link className='navLink' to='/'><h3 >Birdie's Bird Houses</h3></Link>
             <ul id='navLinks'>
                 <Link className='navLink' to='/'><li>Home</li></Link>
                 <Link className='navLink' to='/shop'><li>Shop</li></Link>
