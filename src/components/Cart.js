9bcb006bdb44c351c33310e602e984391a6a0b5a
David Robison <david@robisonweb.dev>
2021-06-15 10:23:13 -0600

    Passed down quantity props to CartCard component.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,9 +2,16 @@ import React from 'react';
 import CartCard from './CartCard';
 
 const Cart = (props) => {
+    const buildCart = () => {
+        const { image, price, title, quantity } = props.cartItems[1];
+
+        return <CartCard title={title} price={price} image={image} quantity={quantity} />
+        // return console.log(props.cartItems[1]);
+    }
+
     return (
         <div className='cart'>
-            <CartCard />
+            {buildCart()}
         </div>
     );
 }
