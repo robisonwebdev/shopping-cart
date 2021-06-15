ce54e1cdc820697fd2d75a4230affa4fdb6d969f
David Robison <david@robisonweb.dev>
2021-06-15 10:09:38 -0600

    Imported CartCard.css. Added src to img and setup with props

--- src/components/CartCard.js
+++ src/components/CartCard.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import QuantityControl from './QuantityControl';
+import '../styles/CartCard.css';
 
 const CartCard = (props) => {
     return (
         <div className='cartCard'>
             <div className='cartCardProduct'>
-                <img className='cartCardImage' alt='' />
+                <img className='cartCardImage' src={props.image} alt='' />
                 <div className='cartCardDetails'>
                     <h3>{props.title}</h3>
                     <p>{props.price}</p>
