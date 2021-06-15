417f4800e2dc6914766fa7370507eb4dd45769ff
David Robison <david@robisonweb.dev>
2021-06-15 10:29:47 -0600

    Moved subtotal math into its own function called getSubtotal. Called getSubtotal in .cartCardSubtotal

--- src/components/CartCard.js
+++ src/components/CartCard.js
@@ -3,13 +3,19 @@ import QuantityControl from './QuantityControl';
 import '../styles/CartCard.css';
 
 const CartCard = (props) => {
+    const { image, title, price, quantity } = props;
+
+    function getSubtotal() {
+        return quantity * price;
+    }
+
     return (
         <div className='cartCard'>
-            <img className='cartCardImage' src={props.image} alt='' />
-            <h3 className='cartCardProduct'>{props.title}</h3>
-            <p className='cartCardPrice'>{`$${props.price}`}</p>
+            <img className='cartCardImage' src={image} alt='' />
+            <h3 className='cartCardProduct'>{title}</h3>
+            <p className='cartCardPrice'>{`$${price}`}</p>
             <QuantityControl />
-            <p className='cartCardSubtotal'>{`$${(props.quantity)*(props.price)}`}</p>
+            <p className='cartCardSubtotal'>{getSubtotal()}</p>
         </div>
     );
 }
