d8d080287b06583b3149a7041d63c41342196f4a
David Robison <david@robisonweb.dev>
2021-06-16 08:31:35 -0600

    Removed function to increase quantity. Commented out both buttons. Passed event.target.value to parseInt. Everytime inputValue is changed, call props.quantityChange with props.id and inputValue.

--- src/components/QuantityControl.js
+++ src/components/QuantityControl.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/QuantityControl.css';
 
 const QuantityControl = (props) => {
+    const [inputValue, setInputValue] = useState(props.quantity);
+
+    useEffect(() => {
+        props.quantityChange(props.id, inputValue);
+    }, [inputValue])
+
     function onSubmit(e) {
         e.preventDefault();
     };
 
     return (
         <form className='quantityControl' onSubmit={onSubmit}>
-            <button onClick={() => props.decrease(props.id)}>-</button>
-            <input type='number' value={props.quantity} />
-            <button onClick={() => props.increase(props.id)}>+</button>
+            {/* <button onClick={() => props.decrease(props.id)}>-</button> */}
+            <input type='number' value={inputValue} onChange={event => setInputValue(parseInt(event.target.value))} />
+            {/* <button onClick={onIncrease}>+</button> */}
         </form>
     );
 }
