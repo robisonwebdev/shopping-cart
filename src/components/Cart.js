e5a0b8d0a2a94576d76cec1361fc4cf3ad520676
David Robison <david@robisonweb.dev>
2021-06-16 06:07:33 -0600

    Setup a table, with thead

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,11 +3,23 @@ import CartCard from './CartCard';
 
 const Cart = (props) => {
     const buildCart = () => {
-        return <CartCard {...props.cartItems[1]} increase={props.increase} decrease={props.decrease} />
+        return <CartCard {...props.cartItems[1]} increase={props.increase} decrease={props.decrease} quantityChange={props.quantityChange} />
     }
 
     return (
         <div className='cart'>
+            <table>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Product</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                        <th>Subtotal</th>
+                        <th></th>
+                    </tr>
+                </thead>
+            </table>
             {buildCart()}
         </div>
     );
