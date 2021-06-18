3b1120e08d86da44a1310e511a333c32d22bb3f1
David Robison <david@robisonweb.dev>
2021-06-18 15:14:21 -0600

    Destructured props. added cartQuantity to cart text.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,15 @@ import React from 'react';
 import '../styles/Navbar.css';
 import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = (props) => {
+    const { cartQuantity } = props;
     return (
         <nav>
             <Link className='navLink' to='/'><h3 >Birdie's Bird Houses</h3></Link>
             <ul id='navLinks'>
                 <Link className='navLink' to='/'><li>Home</li></Link>
                 <Link className='navLink' to='/shop'><li>Shop</li></Link>
-                <Link className='navLink' to='/cart'><li>Cart</li></Link>
+                <Link className='navLink' to='/cart'><li>Cart ( {cartQuantity} )</li></Link>
             </ul>
         </nav>
     );
