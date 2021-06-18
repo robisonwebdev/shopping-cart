b08e8bf7ced9b57ea44faa21be41673137ba565c
David Robison <david@robisonweb.dev>
2021-06-18 15:36:46 -0600

    Adjusted code spacing.

--- src/components/AddToCartButton.js
+++ src/components/AddToCartButton.js
@@ -2,7 +2,6 @@ import React from 'react';
 import '../styles/AddToCartButton.css';
 
 const AddToCartButton = (props) => {
-
     return (
         <div className='addToCartButton'><button onClick={() => props.addToCart(props.addItem.id)}>Add to cart</button></div>
     );
