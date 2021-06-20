7f95e6a82fae903de6e5eb23619563d6be05e6e0
David Robison <david@robisonweb.dev>
2021-06-20 08:54:52 -0600

    Created function called getTotal() getTotal() will run forEach on cartItems, getting subtotal by multipling price and quantity, and saving to variable called total. Returns total after forEach. Called getTotal() between h3. Removed price prop.

--- src/components/TotalPrice.js
+++ src/components/TotalPrice.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
 const TotalPrice = (props) => {
-    const { price } = props;
+    const { cartItems } = props;
+
+    function getTotal() {
+        let total = 0;
+
+        cartItems.forEach(item => {
+            let price = item.price;
+            let quantity = item.quantity;
+            let subtotal = price * quantity;
+
+            total += subtotal;
+        });
+
+        return total;
+    }
+
     return (
         <div className='totalPrice'>
-            <h3>{`Total: ${price}`}</h3>
+            <h3>{`Total: ${getTotal()}`}</h3>
         </div>
     );
 }
