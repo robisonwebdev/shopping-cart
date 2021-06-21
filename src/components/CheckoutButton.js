361ad72279316e027f173456ee64141bc55d3d87
David Robison <david@robisonweb.dev>
2021-06-21 16:15:23 -0600

    Created checkoutAlert function and called it onClick on checkoutButton

--- src/components/CheckoutButton.js
+++ src/components/CheckoutButton.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import '../styles/CheckoutButton.css';
 
+function checkoutAlert() {
+    alert('Thanks for trying my mockup shopping site.');
+}
+
 const CheckoutButton = () => {
     return (
-        <button className='checkoutButton'>Checkout</button>
+        <button className='checkoutButton' onClick={() => {checkoutAlert()}}>Checkout</button>
     );
 }
 
