ee40ca876705f9681b22f113479bc7b90bf9cf96
David Robison <david@robisonweb.dev>
2021-06-22 09:49:49 -0600

    Changed path on all Route componenet

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ const App = () => {
         if (cart.some(item => item.id === id)) {
           let updateProduct = cart.map(item => {
             if (item.id === id) {
-              return {...item, quantity: item.quantity + 1};
+              return { ...item, quantity: item.quantity + 1 };
             }
             return item;
           });
@@ -48,29 +48,29 @@ const App = () => {
   function productQuantityChange(id, value) {
     let updateCart = cart.map(product => {
       if (product.id === id) {
-        return {...product, quantity: value};
+        return { ...product, quantity: value };
       }
       return product;
     });
 
     setCart(updateCart);
   }
-  
+
   return (
     <Router>
       <div className='App'>
         <Navbar cartQuantity={cart.length} />
         <Switch>
-          <Route exact path='/'>
+          <Route exact path='/shopping-cart/'>
             <Home />
           </Route>
-          <Route exact path='/shop'>
+          <Route exact path='/shopping-cart/shop'>
             <Shop />
           </Route>
-          <Route exact path='/cart'>
-            <Cart cartItems={cart} quantityChange={productQuantityChange}/>
+          <Route exact path='/shopping-cart/cart'>
+            <Cart cartItems={cart} quantityChange={productQuantityChange} />
           </Route>
-          <Route exact path='/shop/:id'>
+          <Route exact path='/shopping-cart/shop/:id'>
             <ProductDetails addToCart={addToCart} />
           </Route>
         </Switch>
