5da369c3931e2a7754098cf4a76e2db61840112e
David Robison <david@robisonweb.dev>
2021-06-22 10:48:55 -0600

    Imported HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Shop from './components/Shop';
