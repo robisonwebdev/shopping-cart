bd45155d35bd90da304885e12644477c7fd7d832
David Robison <david@robisonweb.dev>
2021-07-07 12:25:36 -0600

    Refactored code

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -2,10 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/BackButton.css';
 
-const BackButton = (props) => {
-    return (
-        <Link to='/shop'><button className='backButton'>{props.text}</button></Link>
-    );
-}
+const BackButton = (props) => <Link to='/shop'><button className='backButton'>{props.text}</button></Link>
 
 export default BackButton;
