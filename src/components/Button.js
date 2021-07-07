237371517d76e2c4c7b3a9755d38a2d886f499e1
David Robison <david@robisonweb.dev>
2021-07-07 12:46:31 -0600

    Changed BackButton import to Buttons.css

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/BackButton.css';
+import '../styles/Buttons.css';
 
 const Button = ({ text, link, className }) => <Link to={link}><button className={className}>{text}</button></Link>
 
