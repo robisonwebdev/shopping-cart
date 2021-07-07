edbd69a897a8648d4175b97fe8191b81bc232c7c
David Robison <david@robisonweb.dev>
2021-07-07 12:32:36 -0600

    Added Link back around button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/BackButton.css';
 
-const Button = ({ text }) => <button className='backButton'>{text}</button>
+const Button = ({ text }) => <Link to='/shop'><button className='backButton'>{text}</button></Link>
 
 export default Button;
