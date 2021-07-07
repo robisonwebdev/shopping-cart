89adf6bdf5a0a38da6122221c8d41596228eaf65
David Robison <david@robisonweb.dev>
2021-07-07 12:26:42 -0600

    Destructed props

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -2,6 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/BackButton.css';
 
-const BackButton = (props) => <Link to='/shop'><button className='backButton'>{props.text}</button></Link>
+const BackButton = ({ text }) => <Link to='/shop'><button className='backButton'>{text}</button></Link>
 
 export default BackButton;
