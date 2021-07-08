0c24bd5d8f7054fd62eb090790c89f484c3e14aa
David Robison <david@robisonweb.dev>
2021-07-08 14:11:38 -0600

    Destructured props

--- src/components/AddToCartButton.js
+++ src/components/AddToCartButton.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../styles/AddToCartButton.css';
 
-const AddToCartButton = (props) => <div className='addToCartButton'><button onClick={() => props.addToCart(props.addItem.id)}>Add to cart</button></div>
+const AddToCartButton = ({addToCart, addItem}) => <div className='addToCartButton'><button onClick={() => addToCart(addItem.id)}>Add to cart</button></div>
 
 export default AddToCartButton;
