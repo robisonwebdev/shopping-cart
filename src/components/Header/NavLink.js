b8487ba65abcc8cdb25ccddccaa1a2fa76ec719d
robisonwebdev <david@robisonweb.dev>
2022-03-21 12:39:39 -0600

    Moved className from li to Link component.

--- src/components/Header/NavLink.js
+++ src/components/Header/NavLink.js
@@ -4,8 +4,8 @@ import '../../styles/Header/NavLink.css';
 
 const NavLink = ({ linkTo, title  }) => {
     return (
-        <Link to={linkTo}>
-            <li className='nav_link'>{title}</li>
+        <Link className='nav_link' to={linkTo}>
+            <li>{title}</li>
         </Link>
     );
 };
