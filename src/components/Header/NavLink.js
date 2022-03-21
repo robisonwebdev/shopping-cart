72ed0b8ebb1391bfdfaede09bd5f523988414dbf
robisonwebdev <david@robisonweb.dev>
2022-03-21 11:30:27 -0600

    Destrucutred linkTo and title. Added to path on Link, and passed it linkTo prop. Between Link added li and passed it title prop.

--- src/components/Header/NavLink.js
+++ src/components/Header/NavLink.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const NavLink = () => {
+const NavLink = ({ linkTo, title  }) => {
     return (
-        <Link>NavLink</Link>
+        <Link to={linkTo}>
+            <li>{title}</li>
+        </Link>
     );
 };
 
