3b94cc51920bd04f8b2135b9a7683c0bbf03eab7
robisonwebdev <david@robisonweb.dev>
2022-03-21 11:35:04 -0600

    Imported NavLink component. Inisde nav added div with className of nav_title, and ul with className of .nav_links Called NavLink inside of nav_title and nav_links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import NavLink from './NavLink';
 
 const Header = () => {
     return (
-        <header>Header</header>
+        <header>
+            <nav>
+                <div className='nav_title'>
+                    <NavLink linkTo={'#'} title={`Birdie's Bird Houses`} />
+                </div>
+                <ul className='nav_links'>
+                    <NavLink linkTo={'#'} title='Home' />
+                    <NavLink linkTo={'#'} title='Shop' />
+                    <NavLink linkTo={'#'} title='Cart' />
+                </ul>
+            </nav>
+        </header>
     );
 };
 
