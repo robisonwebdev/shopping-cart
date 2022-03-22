49c91db781a47f23a1dfa941fed5730d59062d81
robisonwebdev <david@robisonweb.dev>
2022-03-22 13:10:33 -0600

    Imported Routes, Route, Outlet, Cart, Shop, and Welcome. Replaced main tag with Routes. Setup Route for home, cart, and shop pages. Setup Layout.

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { Routes, Route, Outlet } from 'react-router-dom';
+import Cart from './Cart';
+import Shop from './Shop';
+import Welcome from './Welcome';
 
 const Main = () => {
     return (
-        <main>Main</main>
+        <Routes>
+            <Route path='/' element={<Layout />}>
+                <Route index element={<Welcome />} />
+            </Route>
+
+            <Route path='/cart' element={<Layout />}>
+                <Route index element={<Cart />} />
+            </Route>
+
+            <Route path='/shop' element={<Layout />}>
+                <Route index element={<Shop />} />
+            </Route>
+        </Routes>
     );
 };
+
+const Layout = () => {
+    return (
+        <main>
+            <Outlet />
+        </main>
+    );
+}
 
 export default Main;
