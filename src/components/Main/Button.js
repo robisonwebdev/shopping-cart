8c47a7f470fc69c1bde65e383a0e22530b253d5a
robisonwebdev <david@robisonweb.dev>
2022-03-23 10:14:53 -0600

    Changed Button.css import path

--- src/components/Main/Button.js
+++ src/components/Main/Button.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import '../styles/Buttons.css';
+import '../../styles/Main/Button.css';
 
 const Button = ({ className, linkTo, text }) => {
     return (
