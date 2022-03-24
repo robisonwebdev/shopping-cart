033cab3d398b242b5ac8b44907f413d86ad7bb8c
robisonwebdev <david@robisonweb.dev>
2022-03-24 11:19:48 -0600

    Imported ProductCard and products. Setup mapProducts to map over products and return ProductCard. Added new section to .shop and gave it a className of .shop_products. Called mapProducts inside .shop_products

--- src/components/Main/Shop/Shop.js
+++ src/components/Main/Shop/Shop.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import products from '../../../products/productsInformation';
+import ProductCard from './ProductCard';
 import '../../../styles/Main/Shop/Shop.css';
 
 const Shop = () => {
+    const mapProducts = products.map(product => {
+        return <ProductCard key={product.id} product={product} />
+    });
+
     return (
         <section className='shop'>
             <h1>Bird Houses</h1>
+            <section className='shop_products'>
+                {mapProducts}
+            </section>
         </section>
     );
 };
