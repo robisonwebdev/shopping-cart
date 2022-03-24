24d45ac515a8f54d9a4e6526ca268a33cef15580
robisonwebdev <david@robisonweb.dev>
2022-03-24 11:09:09 -0600

    Changed return div to section. Changed className of .productCard to .product_card

--- src/components/Main/Shop/ProductCard.js
+++ src/components/Main/Shop/ProductCard.js
@@ -3,13 +3,13 @@ import '../styles/ProductCard.css';
 
 const ProductCard = ({ image, title, price }) => {
     return (
-        <div className='productCard'>
+        <section className='product_card'>
             <img className='productImage' src={image} alt={title} />
             <div className='productInfo'>
                 <h3 className='productTitle'>{title}</h3>
                 <p className='productPrice'>{`$${price}`}</p>
             </div>
-        </div>
+        </section>
     );
 }
 
