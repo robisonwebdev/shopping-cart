b34867553ce7508e3bb2f49fe099cb02fc0ed3c7
robisonwebdev <david@robisonweb.dev>
2022-03-24 11:11:50 -0600

    Changed className of .productImage to .product_image Changed className of .productInfo to .product_information Changed className of .productTitle to .product_name Changed className of .productPrice to .product_price

--- src/components/Main/Shop/ProductCard.js
+++ src/components/Main/Shop/ProductCard.js
@@ -4,10 +4,10 @@ import '../styles/ProductCard.css';
 const ProductCard = ({ image, title, price }) => {
     return (
         <section className='product_card'>
-            <img className='productImage' src={image} alt={title} />
-            <div className='productInfo'>
-                <h3 className='productTitle'>{title}</h3>
-                <p className='productPrice'>{`$${price}`}</p>
+            <img className='product_image' src={image} alt={title} />
+            <div className='product_information'>
+                <h3 className='product_name'>{title}</h3>
+                <p className='product_price'>{`$${price}`}</p>
             </div>
         </section>
     );
