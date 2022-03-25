2d9c83d799b1deee14c862829452754d8db0f3aa
robisonwebdev <david@robisonweb.dev>
2022-03-25 11:39:21 -0600

    Changed all ids. Fixed typo on mobile house

--- src/products/productsInformation.js
+++ src/products/productsInformation.js
@@ -1,6 +1,6 @@
 const products = [
     {
-        id: 1,
+        id: 'brown-house',
         image: 'https://images.unsplash.com/photo-1596495551491-734d9ceb1503?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80',
         title: 'Brown House',
         price: '34.00',
@@ -8,7 +8,7 @@ const products = [
         quantity: 0
     },
     {
-        id: 2,
+        id: 'blue-house',
         image: 'https://images.unsplash.com/photo-1601305044331-ee8d0c106675?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80',
         title: 'Blue House',
         price: '24.00',
@@ -16,15 +16,15 @@ const products = [
         quantity: 0
     },
     {
-        id: 3,
+        id: 'mobile-house',
         image: 'https://images.unsplash.com/photo-1532704384331-d96a9862ec11?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
-        title: 'Moblie House',
+        title: 'Mobile House',
         price: '26.00',
         description: 'In fermentum nulla eu est lacinia, at aliquam dui dictum.',
         quantity: 0
     },
     {
-        id: 4,
+        id: 'flower-house',
         image: 'https://images.unsplash.com/photo-1511140016-caa59e0e6d4a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDd8fGJpcmRob3VzZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
         title: 'Flower House',
         price: '25.00',
@@ -32,7 +32,7 @@ const products = [
         quantity: 0
     },
     {
-        id: 5,
+        id: 'tree-house',
         image: 'https://images.unsplash.com/photo-1591101160264-47328d6bdc9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80',
         title: 'Tree House',
         price: '150.00',
@@ -40,7 +40,7 @@ const products = [
         quantity: 0
     },
     {
-        id: 6,
+        id: 'rustic-house',
         image: 'https://images.unsplash.com/photo-1613844272478-9e7901305a90?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1029&q=80',
         title: 'Rustic House',
         price: '34.00',
@@ -48,7 +48,7 @@ const products = [
         quantity: 0
     },
     {
-        id: 7,
+        id: 'white-house',
         image: 'https://images.unsplash.com/photo-1554477378-0d7cfb0e8dc7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
         title: 'White House',
         price: '22.00',
@@ -56,7 +56,7 @@ const products = [
         quantity: 0
     },
     {
-        id: 8,
+        id: 'modern-house',
         image: 'https://images.unsplash.com/photo-1584216928173-e4cfaf153fef?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=864&q=80',
         title: 'Modern House',
         price: '75.00',
