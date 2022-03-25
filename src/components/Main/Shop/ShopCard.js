2826da9856dd5b5c546457277e1df27ec1976828
robisonwebdev <david@robisonweb.dev>
2022-03-25 11:28:37 -0600

    Changed component name and export to ShopCard. Changed .css import path Changed className of .product_card to .shop_card Changed all classNames starting with product to shop_card

--- src/components/Main/Shop/ShopCard.js
+++ src/components/Main/Shop/ShopCard.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../../../styles/Main/Shop/ProductCard.css';
+import '../../../styles/Main/Shop/ShopCard.css';
 
-const ProductCard = ({ product }) => {
+const ShopCard = ({ product }) => {
     const { id, image, price, title } = product;
 
     return (
-        <section className='product_card'>
-            <Link to={`/shop/${id}`}><img className='product_image' src={image} alt={title} /></Link>
-            <div className='product_information'>
-                <Link to={`/shop/${id}`}><h3 className='product_name'>{title}</h3></Link>
-                <p className='product_price'>{`$${price}`}</p>
+        <section className='shop_card'>
+            <Link to={`/shop/${id}`}><img className='shop_card_image' src={image} alt={title} /></Link>
+            <div className='shop_card_information'>
+                <Link to={`/shop/${id}`}><h3 className='shop_card_name'>{title}</h3></Link>
+                <p className='shop_card_price'>{`$${price}`}</p>
             </div>
         </section>
     );
 }
 
-export default ProductCard;
+export default ShopCard;
