02731e4360d36d3b18e206b9c0cf4c6b0b341214
robisonwebdev <david@robisonweb.dev>
2022-03-25 10:47:14 -0600

    Imported Link. Added id to destructured props from product. Put Link component around img and h3. Set Link's component to path.

--- src/components/Main/Shop/ProductCard.js
+++ src/components/Main/Shop/ProductCard.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../../../styles/Main/Shop/ProductCard.css';
 
 const ProductCard = ({ product }) => {
-    const { image, price, title } = product;
+    const { id, image, price, title } = product;
 
     return (
         <section className='product_card'>
-            <img className='product_image' src={image} alt={title} />
+            <Link to={`/shop/${id}`}><img className='product_image' src={image} alt={title} /></Link>
             <div className='product_information'>
-                <h3 className='product_name'>{title}</h3>
+                <Link to={`/shop/${id}`}><h3 className='product_name'>{title}</h3></Link>
                 <p className='product_price'>{`$${price}`}</p>
             </div>
         </section>
