3359eec533d78c2ad91e8f11e0f9e83658154813
robisonwebdev <david@robisonweb.dev>
2022-03-26 18:10:49 -0600

    Removed .product_container but kept its children.

--- src/components/Main/Product/Product.js
+++ src/components/Main/Product/Product.js
@@ -3,10 +3,8 @@ import React from 'react';
 const Product = () => {
     return (
         <section className='product'>
-            <section className='product_container'>
-                <section className='product_image'></section>
-                <section className='product_information'></section>
-            </section>
+            <section className='product_image'></section>
+            <section className='product_information'></section>
         </section>
     );
 };
