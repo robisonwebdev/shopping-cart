242b8911d33b8cf52b9651b782dd5b946cfd7aff
robisonwebdev <david@robisonweb.dev>
2022-03-27 07:20:25 -0600

    Imported useCallback and products. Setup state for product, and set to an initial value of empty object. Setup useParams on id. Setup getProduct function to find product based on id and call setProduct once found. Setup useEffect to call getProduct.

--- src/components/Main/Product/Product.js
+++ src/components/Main/Product/Product.js
@@ -1,11 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import products from '../../../products/productsInformation';
 import '../../../styles/Main/Product/Product.css';
 
 const Product = () => {
+    const [product, setProduct] = useState({});
+    const { id } = useParams();
+
+    const getProduct = useCallback(() => {
+        const findProduct = products.find(value => value.id === id);
+
+        setProduct(findProduct);
+    }, [id]);
+
+    useEffect(() => {
+        getProduct();
+    }, [getProduct]);
+
     return (
         <section className='product'>
             <section className='product_image'>
+                {console.log(product)}
                 <img />
             </section>
             <section className='product_information'>
