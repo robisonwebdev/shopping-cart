92702bcd41dd6fc0da2745c0dc334cde2e597a68
robisonwebdev <david@robisonweb.dev>
2022-03-26 18:08:31 -0600

    Inside return section added new section with className of .product_container Inside .product_container, added two new sections with classNames of .product_image and .product_information

--- src/components/Main/Product/Product.js
+++ src/components/Main/Product/Product.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 const Product = () => {
     return (
-        <section className='product'>Product</section>
+        <section className='product'>
+            <section className='product_container'>
+                <section className='product_image'></section>
+                <section className='product_information'></section>
+            </section>
+        </section>
     );
 };
 
