5c899c210207db1be25f4ab768a16211e1f10797
robisonwebdev <david@robisonweb.dev>
2022-03-29 10:42:23 -0600

    Imported Checkout.js and TotalPrice.js Added h1, section (with className of .cart_items), TotalPrice, and Checkout to .cart

--- src/components/Main/Cart/Cart.js
+++ src/components/Main/Cart/Cart.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Checkout from './Checkout';
+import TotalPrice from './TotalPrice';
 import '../../../styles/Main/Cart/Cart.css';
 
 const Cart = () => {
     return (
-        <section className='cart'>Cart</section>
+        <section className='cart'>
+            <h1>Cart</h1>
+            <section className='cart_items'></section>
+            <TotalPrice />
+            <Checkout />
+        </section>
     );
 };
 
