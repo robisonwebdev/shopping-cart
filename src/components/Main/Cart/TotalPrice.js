3886bd33faba5fb25f829c5ae35efa3960ed942f
robisonwebdev <david@robisonweb.dev>
2022-03-30 07:19:45 -0600

    Changed .totalPrice to .total_price Changed getTotal to an arrow function.

--- src/components/Main/Cart/TotalPrice.js
+++ src/components/Main/Cart/TotalPrice.js
@@ -1,23 +1,23 @@
 import React from 'react';
 
 const TotalPrice = ({ cartItems }) => {
-    function getTotal() {
+    const getTotal = () => {
         let total = 0;
 
         cartItems.forEach(item => {
             let price = item.price;
             let quantity = item.quantity;
             let subtotal = price * quantity;
-
+    
             total += subtotal;
         });
-
+    
         return total.toFixed(2);
-    }
+    };
 
     return (
-        <div className='totalPrice'>
-            <h3>{`Total: $${getTotal()}`}</h3>
+        <div className='total_price'>
+            {/* <h3>{`Total: $${getTotal()}`}</h3> */}
         </div>
     );
 }
