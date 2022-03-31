17df13c27f59bd158be0ad1af79a5994dc838f84
robisonwebdev <david@robisonweb.dev>
2022-03-31 12:22:44 -0600

    Added cartItems and setCartItems to props. Setup addToCart function to determine if product is in cart. If not add product, or if it is increase quentity.

--- src/components/Main/Product/AddToCartButton.js
+++ src/components/Main/Product/AddToCartButton.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import '../../../styles/Main/Product/AddToCartButton.css';
 
-const AddToCartButton = ({ addToCart, product }) => {
+const AddToCartButton = ({ cartItems, product, setCartItems }) => {
+    const addToCart = (prod) => {
+        const isProductInCart = cartItems.some(item => item.id === prod.id);        
+
+        if (!isProductInCart) {
+            prod.quantity = 1;
+            setCartItems(prevState => [...prevState, prod]);
+        };
+
+        if (isProductInCart) {
+            const updateQuantity = cartItems.map(item => {
+                if (item.id === prod.id) {
+                    return { ...item, quantity: item.quantity + 1 };
+                }
+    
+                return item;
+            });
+
+            setCartItems(updateQuantity);
+        };
+    };
+
     return (
         <div className='add_to_cart'>
             <button onClick={() => addToCart(product)}>Add To Cart</button>
