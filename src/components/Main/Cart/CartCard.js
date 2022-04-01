1f5f466b5f81ff712a1688c1a17215f5500d1001
robisonwebdev <david@robisonweb.dev>
2022-04-01 04:13:07 -0600

    Added quantity to destructured props. Changed return section to li. Set input type to number and value to quantity. Added temp number to last p tag.

--- src/components/Main/Cart/CartCard.js
+++ src/components/Main/Cart/CartCard.js
@@ -2,16 +2,16 @@ import React from 'react';
 import '../../../styles/Main/Cart/CartCard.css';
 
 const CartCard = ({ item }) => {
-    const { image, price, title } = item;
+    const { image, price, quantity, title } = item;
 
     return (
-        <section className='cart_card'>
+        <li className='cart_card'>
             <img src={image} alt={title} />
             <h4>{title}</h4>
             <p>{price}</p>
-            <input />
-            <p></p>
-        </section>
+            <input type='number' value={quantity} />
+            <p>$23.00</p>
+        </li>
     );
 };
 
