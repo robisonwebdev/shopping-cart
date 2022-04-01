58d0368ecbe1e63e92e72bdc953e720955ab877b
robisonwebdev <david@robisonweb.dev>
2022-04-01 03:55:05 -0600

    Imported CartCard component. Setup mapCartItems to map over cartItems and return CartCard component. Called mapCartItems in .cart_items section.

--- src/components/Main/Cart/Cart.js
+++ src/components/Main/Cart/Cart.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import CartCard from './CartCard';
 import Checkout from './Checkout';
 import TotalPrice from './TotalPrice';
 import '../../../styles/Main/Cart/Cart.css';
 
 const Cart = ({ cartItems, setCartItems }) => {
+    const mapCartItems = cartItems.map(item => {
+        return <CartCard key={item.id} item={item} />;
+    });
+
     return (
         <section className='cart'>
             <h1>Cart</h1>
-            <section className='cart_items'></section>
+            <section className='cart_items'>
+                {mapCartItems}
+            </section>
             <TotalPrice />
             <Checkout />
         </section>
