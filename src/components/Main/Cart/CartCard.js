9305949f9f08182e75daac4fbc305fdc7a1b894b
robisonwebdev <david@robisonweb.dev>
2022-04-01 03:56:47 -0600

    Added item to props. Destructured props from item. Added img, h4, p, and input tags to return section. Passed props to img, h4, and p tags.

--- src/components/Main/Cart/CartCard.js
+++ src/components/Main/Cart/CartCard.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
-const CartCard = () => {
+const CartCard = ({ item }) => {
+    const { image, price, title } = item;
+
     return (
-        <section>CartCard</section>
+        <section className='cart_card'>
+            <img src={image} alt={title} />
+            <h4>{title}</h4>
+            <p>{price}</p>
+            <input />
+            <p></p>
+        </section>
     );
 };
 
