5a8739a943e20f3151dd9bfb636e93254337f4d8
robisonwebdev <david@robisonweb.dev>
2022-04-03 11:59:40 -0600

    Setup getSubtotal function to compute price and return. Called getSubtotal in last td.

--- src/components/Main/Cart/CartCard.js
+++ src/components/Main/Cart/CartCard.js
@@ -4,13 +4,17 @@ import '../../../styles/Main/Cart/CartCard.css';
 const CartCard = ({ item }) => {
     const { image, price, quantity, title } = item;
 
+    const getSubtotal = () => {
+        return (quantity * price).toFixed(2);
+    };
+
     return (
         <tr className='cart_card'>
             <td><img src={image} alt={title} /></td>
             <td className='td_product'><h4>{title}</h4></td>
             <td><p>{price}</p></td>
             <td><input type='number' value={quantity} /></td>
-            <td><p>$23.00</p></td>
+            <td><p>{getSubtotal()}</p></td>
         </tr>
     );
 };
