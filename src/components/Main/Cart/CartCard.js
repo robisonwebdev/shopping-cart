6dee7dd3f18977dabd501942e2c59e224e3521ba
robisonwebdev <david@robisonweb.dev>
2022-04-04 11:52:22 -0600

    Imported useEffect and useState. Added cartItems and setCartItems to props. Setup state for itemQuantity and set to an initial value of quantity. Setup onChange function to update setItemQuantity. Setup updateCart function to update item quantity and push changed to setCartItems. Setup useEffect to run when itemQuantity changes, calling updateCart. Added onChanged event to input, calling onChange function. Changed value in input to accept itemQuantity.

--- src/components/Main/Cart/CartCard.js
+++ src/components/Main/Cart/CartCard.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../../styles/Main/Cart/CartCard.css';
 
-const CartCard = ({ item }) => {
+const CartCard = ({ cartItems, item, setCartItems }) => {
     const { image, price, quantity, title } = item;
+    const [itemQuantity, setItemQuantity] = useState(quantity);
+
+    const onChange = (event) => {
+        setItemQuantity(parseInt(event.target.value));
+    };
 
     const getSubtotal = () => {
         return (quantity * price).toFixed(2);
     };
+
+    const updateCart = () => {
+        const newItemQuantity = cartItems.map(obj => {
+            if (obj.id === item.id) {
+                return {...obj, quantity: itemQuantity};
+            };
+
+            return obj;
+        });
+
+        setCartItems(newItemQuantity);
+    };
+
+    useEffect(() => {
+        updateCart();
+    }, [itemQuantity]);
 
     return (
         <tr className='cart_card'>
             <td><img src={image} alt={title} /></td>
             <td className='td_product'><h4>{title}</h4></td>
             <td><p>{price}</p></td>
-            <td><input type='number' value={quantity} /></td>
+            <td><input onChange={onChange} type='number' value={itemQuantity} /></td>
             <td><p>{getSubtotal()}</p></td>
         </tr>
     );
